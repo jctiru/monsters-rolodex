942437e6bda723f6242be8af8fa541e2dcb806be
jctiru <jonathanchristiantiru@gmail.com>
2019-11-19 18:07:08 +1000

    Updates

---
+++ precache-manifest.bf0345ae9d8a11a4cff2ac09e1c32cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fef0179a213b6923ec051dcc079240",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f0e449b9a59295c1d1da",
+    "url": "/monsters-rolodex/static/css/main.0811ade4.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/monsters-rolodex/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "f0e449b9a59295c1d1da",
+    "url": "/monsters-rolodex/static/js/main.a166044d.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
